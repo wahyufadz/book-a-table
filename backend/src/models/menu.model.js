de69c25cc9cb26a1220a2607124b1977de5be980
Fadzar Wahyu <wahyufadz@gmail.com>
2019-09-10 09:43:36 +0800

    add .editorconfig and make adjustment

--- backend/src/models/menu.model.js
+++ backend/src/models/menu.model.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const MenuSchema = mongoose.Schema(
-    {
-        Menu: String,
-        content: String
-    }, {
-        timestamps: true
-    });
+const MenuSchema = mongoose.Schema({
+    Menu: String,
+    content: String
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('Menu', MenuSchema);
